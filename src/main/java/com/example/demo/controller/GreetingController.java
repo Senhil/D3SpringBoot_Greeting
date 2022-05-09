694a28c30c2695e3bd4bd6f21c557e8c952ea6d7
SK <senthilptj@gmail.com>
2022-05-09 10:45:37 +0530

    Added: Greeting Message and CURL Operation in Spring Boot

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Greeting;
+import com.example.demo.model.User;
+import com.example.demo.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+//import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+@RequestMapping("")
+public class GreetingController
+{
+    private static final String template = "%s";
+
+    @Autowired
+    private IGreetingService iGreetingService;
+
+    @GetMapping(value = {"/home"})
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name)
+    {
+        User user = new User();
+        user.setFirstName(name);
+        return iGreetingService.addGreeting(user);
+    }
+
+    @PostMapping("/fullName")
+    public Greeting sayWow(@RequestBody User user) {
+        return iGreetingService.addGreeting(user);
+    }
+
+    @GetMapping("/find/{id}")
+    public Optional<Greeting> greeting(@PathVariable(value = "id") long id)
+    {
+        return iGreetingService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<Greeting> getAll(){
+        return iGreetingService.getAll();
+    }
+
+    @PutMapping("/editGreeting/{id}")
+    public Optional<Greeting> editGreetingById(@PathVariable("id") long id, @RequestParam(value = "name") String name) {
+        return iGreetingService.editGreetingById(id, name);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteByID(@PathVariable("id") long id) {
+        iGreetingService.delete(id);
+        return "Deleted Succesfully";
+    }
+}
